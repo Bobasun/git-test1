446e4858b79ea8ebb701baccec6ad3029e1fb942
Vova <dp-ptcstd-48@ptc.local>
2019-10-09 13:48:30 +0300

    B1_2

--- src/Master2.java
+++ src/Master2.java
@@ -4,6 +4,8 @@ public class Master2 {
         System.out.println("4");
         int i = myFunction(1, "Hey");
         System.out.println(i);
+        System.out.println("b1");
+        System.out.println("b1_2");
     }
 
     private static int myFunction(int i, String hey) {
