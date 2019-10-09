5f94a23691d31ea8a7ca6db4b3d4cd3bb443eca1
Vova <dp-ptcstd-48@ptc.local>
2019-10-09 11:53:52 +0300

    M_6

--- src/Master2.java
+++ src/Master2.java
@@ -2,5 +2,12 @@ public class Master2 {
     public static void main(String[] args) {
         System.out.println("3");
         System.out.println("4");
+        int i = myFunction(1, "Hey");
+        System.out.println(i);
+    }
+
+    private static int myFunction(int i, String hey) {
+        System.out.println(hey);
+        return  i;
     }
 }
